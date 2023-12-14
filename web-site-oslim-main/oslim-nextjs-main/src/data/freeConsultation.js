bd0c1ba2f41489c10f0d0f7adc1014607e6d23b8
MasterDesigner1 <151040538+MasterDesigner1@users.noreply.github.com>
2023-12-14 13:26:04 +0300

    Delete web-site-oslim-main directory

--- web-site-oslim-main/oslim-nextjs-main/src/data/freeConsultation.js
+++
@@ -1,7 +0,0 @@
-export const freeConsultation = {
-  title: "Daha Detaylı Bilgi Almak İçin",
-  titleHighlight: "FREE",
-  phone: "+90 (216) 700 1833",
-  phoneHref: "+902167001833",
-  email: "buro@koksalpartners.com",
-};
