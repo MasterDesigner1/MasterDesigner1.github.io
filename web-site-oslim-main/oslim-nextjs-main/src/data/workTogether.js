6c89ef76ad3296322f736713ba56ecf9f6fc9e79
MasterDesigner1 <oguzhan.saman@kpveri.com>
2023-12-14 13:29:17 +0300

    İlk

---
+++ web-site-oslim-main/oslim-nextjs-main/src/data/workTogether.js
@@ -0,0 +1,31 @@
+import image1 from "@/images/resources/work-together-img-1.jpg";
+import image2 from "@/images/resources/work-together-img-2.jpg";
+import parsonImage from "@/images/resources/work-together-person-img.jpg";
+import image3 from "@/images/resources/work-together-two-img-1.jpg";
+import shape from "@/images/shapes/work-together-shape-1.png";
+
+export const workTogether = {
+  shape,
+  image1,
+  image2,
+  tagline: "bizi tanıyın",
+  title: "Neden Bizimle Çalışmalısınız?",
+  text: "Köksal&Partners, kişisel verilerin korunması hukuku, şirketler hukuku, bilişim hukuku, fikri mülkiyet hukuku, sözleşmeler hukuku ve uyuşmazlık çözümleri de dahil olmak üzere hukukun çok çeşitli alanlarında yerli ve yabancı müvekkillerine her türlü hukuki danışmanlık ve avukatlık hizmeti sağlamakta olan bir avukatlık ortaklığıdır. Eylül 2005’te Ankara'da temellerini atan ofisimiz, Eylül 2011 itibariyle merkezini İstanbul'a taşımış olup bu tarihten bu yana da faaliyetlerini İstanbul, Ankara ve İzmir’de yürütmeye devam etmektedir.",
+  points: [
+    "Nsectetur cing elit.",
+    "Suspe ndisse suscipit sagittis leo.",
+    "Entum estibulum dignissim posuere.",
+    "Donec tristique ante vel sem dictum rhoncus.",
+  ],
+  parsonImage,
+  personName: "Kevin Martin",
+};
+
+export const workTogetherTwo = {
+  image: image3,
+  tagline: "bizi tanıyın",
+  title: "Neden Bizimle Çalışmalısınız?",
+  text: "Köksal&Partners, kişisel verilerin korunması hukuku, şirketler hukuku, bilişim hukuku, fikri mülkiyet hukuku, sözleşmeler hukuku ve uyuşmazlık çözümleri de dahil olmak üzere hukukun çok çeşitli alanlarında yerli ve yabancı müvekkillerine her türlü hukuki danışmanlık ve avukatlık hizmeti sağlamakta olan bir avukatlık ortaklığıdır. Eylül 2005’te Ankara'da temellerini atan ofisimiz, Eylül 2011 itibariyle merkezini İstanbul'a taşımış olup bu tarihten bu yana da faaliyetlerini İstanbul, Ankara ve İzmir’de yürütmeye devam etmektedir.",
+  text2: "Köksal&Partners bünyesinde faaliyet gösteren avukatlarımız, kuruluşundan bu yana teknolojinin tüm imkanlarını kullanarak ve Avukatlık Kanunu ile avukatlık meslek kurallarına bağlı bir şekilde müvekkillerine her türlü hukuki desteği sağlamaya devam etmektedir. Müvekkillerimizin geri bildirimlerinden hareketle, müvekkillerimizin her türlü hukuki sorun veya görüş talepleri ile mevzuata uyum süreçlerini dinamik ve etkin bir avukat kadrosuyla titizlikle takip ediyor; ticari hayatta müvekkillerimizin değişken ihtiyaçlarına ilişkin olarak her türlü hukuki süreci müvekkillerimizin en avantajına olacak şekilde sonuçlandırmayı hedefliyoruz. Köksal&Partners için her gün değişen ve gelişen teknolojiye uyum sağlamak ve hukuki faaliyetlerini teknoloji ile entegre etmek temel amaçlardan biridir. Bu kapsamda sürekli danışmanlık hizmeti verdiğimiz şirketlerin ve kurumsal müvekkillerimizin de kendi dosyalarını ve iş süreçlerini elektronik ortamdan takip edebilmelerini sağlıyoruz. Temel vizyonumuz, hukuki danışmanlık ve avukatlık hizmetlerimizin kalitesini devam ettirmek ve müvekkillerimizin değişen hukuki koşullara her zaman hazırlıklı olmasını sağlamaktır.",
+  
+};
